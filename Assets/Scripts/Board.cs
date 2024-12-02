d3b80060f6ae06bd1d0fbe9e2b63e11fb90d026d
penguinjean0421 <penguinjean0421@gmail.com>
2024-12-03 00:24:42 +0900

    Scene Setup ~ Spawning / Setting

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Board : MonoBehaviour
+{
+    public Tilemap tileMap { get; private set; }
+    public Piece activePiece { get; private set; }
+    public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition;
+
+    private void Awake()
+    {
+        this.tileMap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
+        for (int i = 0; i < this.tetrominos.Length;i ++)
+        {
+            this.tetrominos[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominos.Length);
+        TetrominoData data = this.tetrominos[random];
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(this.activePiece);
+
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i ++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tileMap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
+}
