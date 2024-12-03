907f73a1214d9cefa7d22afa1e3f68e0a2e473d9
penguinjean0421 <penguinjean0421@gmail.com>
2024-12-03 11:24:31 +0900

    Movement && Rotation

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,6 +6,15 @@ public class Board : MonoBehaviour
     public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
     public Vector3Int spawnPosition;
+    public Vector2Int boardSize = new Vector2Int(10, 20);
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, boardSize);
+        }
+    }
 
     private void Awake()
     {
@@ -38,5 +47,33 @@ public class Board : MonoBehaviour
             Vector3Int tilePosition = piece.cells[i] + piece.position;
             this.tileMap.SetTile(tilePosition, piece.data.tile);
         }
+    }
+
+    public void Clear(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i ++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            this.tileMap.SetTile(tilePosition, null);
+        }
+    }
+
+    public bool IsValidPosition(Piece piece, Vector3Int position) 
+    {
+        RectInt bounds = this.Bounds;
+        for (int i = 0; i < piece.cells.Length; i ++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + position;
+
+            if(!bounds.Contains((Vector2Int)tilePosition))
+            {
+                return false;
+            }
+            if(this.tileMap.HasTile(tilePosition))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
